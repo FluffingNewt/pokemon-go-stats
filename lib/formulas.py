edc6ab5b583ad3cfa4a3f4752b5eb3b0cc917180
Davis Guest <davisdevice@gmail.com>
2024-05-21 18:52:24 +0000

    file restructures, finished both move databases, started pokemon database

---
+++ lib/formulas.py
@@ -0,0 +1,16 @@
+import math
+
+# Calculates the max cp of a pokemon given its base stats, assumes lvl 50 and 100% ivs
+def calc_max_cp(attack, defense, hp):
+    cpm = 0.84029999 # lvl 50 cpm, at lvl 40 = 0.7903
+    cp = ((attack + 15) * pow(defense + 15, 0.5) * pow(hp + 15, 0.5) * pow(cpm, 2)) / 10
+    return math.floor(max(10, cp))
+
+
+
+
+
+
+
+
+##print(calc_max_cp(198,189,190))
